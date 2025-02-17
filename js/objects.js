079345e98f16751ff487fdcbf7185eb02233abc2
MajorKujo <151047864+MajorKujo@users.noreply.github.com>
2025-02-17 16:24:57 +0530

    js training first day

---
+++ js/objects.js
@@ -0,0 +1,8 @@
+// let Person = {
+//     Name:'Mishal',
+//     Age:20,
+//     Address:{Place:'Kothamangalam',State:'Kerala'}
+// };
+// console.log(Person);
+// console.log(Person.Name);
+// console.log(Person.Address.State);
